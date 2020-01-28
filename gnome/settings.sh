03a7edb0d8b2dff518213d94d4a32ad46bacef73
Sean Buckley <sean.bck@gmail.com>
2020-01-27 23:29:32 -0500

    Revert "make taskbar smaller (same size as default gnome top bar)"
    This reverts commit c100d9dc70e1e996fb141fa0be1b58234558d5db.

--- gnome/settings.sh
+++ gnome/settings.sh
@@ -36,9 +36,8 @@ gsettings set org.gnome.desktop.privacy remove-old-trash-files true
 
 gsettings set org.gnome.desktop.interface enable-hot-corners false
 
-dconf write /org/gnome/shell/extensions/dash-to-panel/dot-position "'LEFT'"
-dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 27
-dconf write /org/gnome/shell/extensions/dash-to-panel/appicon-padding 0
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 32
+dconf write /org/gnome/shell/extensions/dash-to-panel/appicon-padding 2
 dconf write /org/gnome/shell/extensions/dash-to-panel/group-apps false
 dconf write /org/gnome/shell/extensions/dash-to-panel/isolate-workspaces true
 dconf write /org/gnome/shell/extensions/dash-to-panel/show-window-previews false
