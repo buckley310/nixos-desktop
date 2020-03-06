c57b030c1dbbaf664f2b6f7bb3645656cb6e7b88
Sean Buckley <sean.bck@gmail.com>
2020-03-05 21:38:49 -0500

    remove showdesktop button

--- gnome/settings.sh
+++ gnome/settings.sh
@@ -41,4 +41,5 @@ dconf write /org/gnome/shell/extensions/dash-to-panel/appicon-padding 2
 dconf write /org/gnome/shell/extensions/dash-to-panel/group-apps false
 dconf write /org/gnome/shell/extensions/dash-to-panel/isolate-workspaces true
 dconf write /org/gnome/shell/extensions/dash-to-panel/show-window-previews false
+dconf write /org/gnome/shell/extensions/dash-to-panel/show-showdesktop-button false
 gsettings set org.gnome.shell enabled-extensions "['dash-to-panel@jderose9.github.com']"
