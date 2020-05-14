8b9b8121341259273d0574fa6712cef3cdd96dac
Sean Buckley <sean.bck@gmail.com>
2020-05-14 13:18:20 -0400

    add numix-icon-theme

--- gnome/default.nix
+++ gnome/default.nix
@@ -3,6 +3,7 @@
   environment = {
     gnome3.excludePackages = with pkgs.gnome3; [ epiphany vinagre gnome-software ];
     systemPackages = with pkgs; [
+      numix-icon-theme
       gnome3.gnome-tweaks gnome3.gnome-boxes gnomeExtensions.sound-output-device-chooser
       (callPackage ./bottom-panel.nix {})
       (writeScriptBin "red" ''
