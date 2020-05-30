be435892fdf833b2d4677a00bf0b4c399d5db3a4
Sean Buckley <sean.bck@gmail.com>
2020-05-30 01:36:50 -0400

    adjust taskbar padding

--- gnome/dash-to-panel.sh
+++ gnome/dash-to-panel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
-dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 32
-dconf write /org/gnome/shell/extensions/dash-to-panel/appicon-padding 2
+dconf write /org/gnome/shell/extensions/dash-to-panel/appicon-margin 4
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 40
 dconf write /org/gnome/shell/extensions/dash-to-panel/group-apps false
 dconf write /org/gnome/shell/extensions/dash-to-panel/isolate-workspaces true
 dconf write /org/gnome/shell/extensions/dash-to-panel/show-window-previews false
